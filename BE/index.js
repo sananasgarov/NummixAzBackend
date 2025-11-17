f8144efe1ee797fc6621bc5330991a3aefadb228
Sanan Asgarov <prog58816@gmail.com>
2025-11-17 23:55:02 +0400

    fix: configure CORS to allow requests from the specified origin

--- BE/index.js
+++ BE/index.js
@@ -10,7 +10,11 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+  {
+    origin: "https://www.nummix.az/",
+  }
+));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
