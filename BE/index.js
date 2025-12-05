4047105aa0f16bd6996d375e78cd7cbdbee9b317
Sanan Asgarov <prog58816@gmail.com>
2025-12-05 19:24:22 +0400

    l

--- BE/index.js
+++ BE/index.js
@@ -10,10 +10,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({
-  origin: true,
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
