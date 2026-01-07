35aa9083bb852f24125352e5aae6298bea9e48ca
sananasgarov <prog58816@gmail.com>
2026-01-08 00:15:38 +0400

    fix: update email transporter configuration to use Gmail service

--- BE/index.js
+++ BE/index.js
@@ -124,9 +124,7 @@ app.get("/api/auth/verify", authenticateToken, async (req, res) => {
 });
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
